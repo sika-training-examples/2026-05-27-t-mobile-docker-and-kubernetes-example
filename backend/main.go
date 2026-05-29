54f232b3928e5fcab3e6b0ff136b32815d25c3aa
Ondrej Sika <ondrej@ondrejsika.com>
2026-05-29 16:39:45 +0200

    feat(backend): update version to t-mobile-v3

--- backend/main.go
+++ backend/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"os"
 	"github.com/ondrejsika/counter/pkg/server"
+	"github.com/ondrejsika/counter/version"
 )
 
 func main() {
 	os.Setenv("EXTRA_TEXT", "Hello T-Mobile")
 	os.Setenv("API_ONLY", "1")
+	version.Version = "t-mobile-v3"
 	server.Server(server.ServerOptions{})
 }
