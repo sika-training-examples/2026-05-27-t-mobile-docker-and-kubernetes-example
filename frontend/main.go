0ea70376f85b77450d9e3e9f8e15d37eddc5ac6e
Ondrej Sika <ondrej@ondrejsika.com>
2026-05-29 16:40:39 +0200

    feat(frontend): update version to t-mobile-v3

--- frontend/main.go
+++ frontend/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"os"
 	"github.com/ondrejsika/counter-frontend-go/pkg/server"
+	"github.com/ondrejsika/counter-frontend-go/version"
 )
 
 func main() {
 	os.Setenv("FONT_COLOR", "white")
 	os.Setenv("BACKGROUND_COLOR", "#EC018D")
+	version.Version = "t-mobile-v3"
 	server.Server()
 }
